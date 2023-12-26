541b4b42016c12130dcebede1639d6f39da76e3f
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-26 12:23:24 -0500

    u

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,108 +1,50 @@
-const router = require('express').Router()
-const User = require('../users/users-model')
-const bcrypt = require('bcryptjs')
-
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const User = require('../users/users-model');
 const {
-  checkPasswordLength,
+  checkUsernameFree,
   checkUsernameExists,
-  checkUsernameFree,
-} = require('./auth-middleware')
-
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
-
-
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
-router.post('/register',
   checkPasswordLength,
-  checkUsernameFree,
-  (req, res, next) => {
-    const { username, password } = req.body
-    const hash = bcrypt.hashSync(password, 8)
+} = require('../middleware/auth-middleware');
 
-    User.add({ username, password: hash })
-      .then(saved => {
-        res.status(201).json(saved)
-      })
-      .catch(next)
-  })
+router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+    const newUser = await User.add({ username, password: hash });
 
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
+    res.status(200).json(newUser);
+  } catch (err) {
+    next(err);
   }
+});
 
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
+router.post('/login', checkUsernameExists, async (req, res, next) => {
+  try {
+    const { password } = req.body;
+    if (req.user && bcrypt.compareSync(password, req.user.password)) {
+      req.session.user = req.user;
+      res.status(200).json({ message: `Welcome ${req.user.username}!` });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (err) {
+    next(err);
   }
- */
+});
 
-router.post('/login', checkUsernameExists, (req, res, next) => {
-  const { password } = req.body
-  if (bcrypt.compareSync(password, req.user.password)) {
-    req.session.user = req.user
-    res.json({ message: `Welcome ${req.user.username}` })
-  } else {
-    next({ status: 401, message: 'Invalid credentials' })
-  }
-})
-/**
-  3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
 router.get('/logout', (req, res, next) => {
-  if (req.session.user) {
+  if (req.session) {
     req.session.destroy(err => {
-      if(err) {
-        next(err)
+      if (err) {
+        next(err);
       } else {
-        res.json({ message: "logged out"})
+        res.status(200).json({ message: 'logged out' });
       }
-    })
+    });
   } else {
-    res.json({ message: 'no session' })
+    res.status(200).json({ message: 'no session' });
   }
-})
+});
 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
-
-
-module.exports = router
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
-
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
-
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
+const session = require('express-session');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+// Configure express-session middleware
+server.use(session({
+  name: 'chocolatechip', // name of the cookie
+  secret: 'keep it secret, keep it safe', // used for cookie encryption
+  cookie: {
+    maxAge: 1000 * 60 * 60, // valid for 1 hour
+  },
+  httpOnly: true, // prevent client-side JS from accessing the cookie
+  secure: false, // set to true in production, requires https
+  resave: false,
+  saveUninitialized: false, // don't create a session until something is stored
+}));
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
